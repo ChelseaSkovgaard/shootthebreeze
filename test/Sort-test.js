b4b714391624a92dee79a364776a5d0d7fbc19ba
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-10-19 15:18:38 -0600

    set up testing files

---
+++ test/Sort-test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { shallow, mount, render } from 'enzyme';
+import { assert } from 'chai';
+
+import Sort from '../lib/components/Sort';
---
+++ test/User-List-test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { shallow, mount, render } from 'enzyme';
+import { assert } from 'chai';
+
+import UserList from '../lib/components/User-List';
