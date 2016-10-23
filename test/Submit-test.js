0394364f8ce03055889994599dc4ac1008f5de03
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-10-23 12:43:53 -0600

    submit button disabled when character count over 140

---
+++ test/Submit-test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { shallow, mount, render } from 'enzyme';
+import { assert, expect } from 'chai';
+import Locus from 'locus';
+import SubmitButton from '../lib/components/Submit-Button';
+
+describe('SubmitButton', () => {
+
+  it('renders as a <button>', () => {
+    const wrapper = shallow(<SubmitButton />)
+    assert.equal(wrapper.type(), 'button');
+  });
+});
